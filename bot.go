4a3150a56b68e8f7be1292802d4af5de4bcc9bec
Nabiel Kandiel <nabkan5@gmail.com>
2020-07-13 03:56:47 -0400

    shows stonk image

---
+++ bot.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+	"os/signal"
+	"strings"
+	"syscall"
+
+	"github.com/bwmarrin/discordgo"
+)
+
+// Variables used for command line parameters
+var (
+	Token string
+)
+
+func init() {
+
+	flag.StringVar(&Token, "t", "", "Bot Token")
+	flag.Parse()
+}
+
+func main() {
+	fmt.Println(Token)
+	discord, err := discordgo.New("Bot " + Token)
+	if err != nil {
+		fmt.Println("Error creating Discord session: ", err)
+		return
+	}
+
+	discord.AddHandler(messageCreate)
+
+	// Open a websocket connection to Discord and begin listening.
+	err = discord.Open()
+	if err != nil {
+		fmt.Println("error opening connection,", err)
+		return
+	}
+
+	// Wait here until CTRL-C or other term signal is received.
+	fmt.Println("Bot is now running.  Press CTRL-C to exit.")
+	sc := make(chan os.Signal, 1)
+	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
+	<-sc
+
+	// Cleanly close down the Discord session.
+	discord.Close()
+}
+
+// This function will be called (due to AddHandler above) every time a new
+// message is created on any channel that the authenticated bot has access to.
+func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
+
+	// Ignore all messages created by the bot itself
+	// This isn't required in this specific example but it's a good practice.
+	if m.Author.ID == s.State.User.ID {
+		return
+	}
+	if strings.Contains(strings.ToLower(m.Content), "stocks") {
+		data, err := os.Open("STONKS.jpg")
+		if err != nil {
+			fmt.Println("Error reading file: ", err.Error())
+			return
+		}
+		s.ChannelFileSend(m.ChannelID, "STONKS.jpg", data)
+	}
+
+}
